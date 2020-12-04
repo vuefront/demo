2ae5d6e48294b141c55e9ce5a68916915e6bcfc5
Alex Krasnij <krasaler@gmail.com>
2020-12-04 12:59:24 +0200

    feat: add new components

--- pages/molecules.vue
+++ pages/molecules.vue
@@ -16,30 +16,31 @@
           <vf-m-dropdown-item>Item 3</vf-m-dropdown-item>
       </vf-m-dropdown>
       <vf-m-dropdown variant="link" right>
-                    <template slot="button-content">
-                        <vf-a-icon icon="account-circle-outline" />
-                        Label
-                    </template>
-                    <vf-m-dropdown-item>Item 1</vf-m-dropdown-item>
-                    <vf-m-dropdown-item>Item 2</vf-m-dropdown-item>
-                    <vf-m-dropdown-item>Item 3</vf-m-dropdown-item>
-                </vf-m-dropdown>
+          <template slot="button-content">
+              <vf-a-icon icon="account-circle-outline" />
+              Label
+          </template>
+          <vf-m-dropdown-item>Item 1</vf-m-dropdown-item>
+          <vf-m-dropdown-item>Item 2</vf-m-dropdown-item>
+          <vf-m-dropdown-item>Item 3</vf-m-dropdown-item>
+      </vf-m-dropdown>
     </div>
-    <div>
-    <vf-m-input-group
-      v-for="size in ['sm','','lg']"
-      :key="size"
-      :size="size"
-      class="mb-3"
-      prepend="Label"
-    >
-      <vf-a-input></vf-a-input>
-      <template #append>
-        <vf-a-button size="sm" text="Button" variant="success">Button</vf-a-button>
-      </template>
-    </vf-m-input-group>
+    <vf-a-heading class="text-center mt-4">INPUT GROUP</vf-a-heading>
+    <div class="text-center my-8">
+      <vf-m-input-group
+        v-for="size in ['sm','','lg']"
+        :key="size"
+        :size="size"
+        class="mb-3"
+        prepend="Label"
+      >
+        <vf-a-input></vf-a-input>
+        <template #append>
+          <vf-a-button size="sm" text="Button" variant="success">Button</vf-a-button>
+        </template>
+      </vf-m-input-group>
     </div>
-    <div>
+    <div class="text-center my-8">
       <vf-m-input-group>
         <template #prepend>
           <vf-m-dropdown text="Dropdown" variant="info">
@@ -57,6 +58,36 @@
           </vf-m-dropdown>
         </template>
       </vf-m-input-group>
+    </div>
+    <vf-a-heading class="text-center mt-4">BUTTON GROUP</vf-a-heading>
+    <div class="text-center my-8">
+      <vf-m-button-group>
+        <vf-a-button>Button 1</vf-a-button>
+        <vf-a-button>Button 2</vf-a-button>
+        <vf-a-button>Button 3</vf-a-button>
+      </vf-m-button-group>
+    </div>
+    <div class="text-center my-8">
+      <vf-m-button-group size="sm">
+        <vf-a-button>Left</vf-a-button>
+        <vf-a-button>Middle</vf-a-button>
+        <vf-a-button>Right</vf-a-button>
+      </vf-m-button-group>
+    </div>
+    <div class="text-center my-8">
+      <vf-m-button-group size="lg">
+        <vf-a-button>Left</vf-a-button>
+        <vf-a-button>Middle</vf-a-button>
+        <vf-a-button>Right</vf-a-button>
+      </vf-m-button-group>
+    </div>
+    <vf-a-heading class="text-center mt-4">LIST GROUP</vf-a-heading>
+    <div class="text-center my-8">
+      <vf-m-list-group>
+          <vf-m-list-group-item active to="#">List item one</vf-m-list-group-item>
+          <vf-m-list-group-item to="#">List item two</vf-m-list-group-item>
+          <vf-m-list-group-item to="#">List item tree</vf-m-list-group-item>
+      </vf-m-list-group>
     </div>
   </vf-m-container>
 </template>
